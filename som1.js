8eee3f0fd2cb59f3624aa31d09dc3052ef00ede6
mathis-de-brouwer <145472006+mathis-de-brouwer@users.noreply.github.com>
2023-09-25 12:12:56 +0100

    som

---
+++ som1.js
@@ -0,0 +1,26 @@
+import * as readline from 'node:readline/promises';
+import{stdin as input, stdout as output} from 'node:process';
+const userInput = readline.createInterface({input, output});
+
+
+
+
+
+/*
+let som;
+let getal1 = 51;
+let getal2 = 7.5;
+
+som = getal1 + getal2;
+console.log(som);
+
+
+/*let jeGetal = parseFloat(await userInput.question('Geef een getal in:'));
+console.log('Je getal maal 2 is gelijk aan '+ (jeGetal + 5));
+*/
+
+
+
+let jegetal = parseFloat(await userInput.question('Geef een getal in:'));
+console.log('de som van je getal en 2 is:'+ (jegetal + 2));
+
---
+++ toetsvraag.mjs
@@ -0,0 +1,18 @@
+
+// Begin van boilerplate dat je even mag vergeten
+import * as readline from 'node:readline/promises';
+import{stdin as input, stdout as output} from 'node:process';
+const userInput = readline.createInterface({input, output});
+// Einde van boilerplate dat je even mag vergeten
+
+
+let today = await userInput.question('Welke dag van de week is het vandaag? ');
+console.log(today);
+
+let day = await userInput.question('De hoeveelste dag van de maand is het? ');
+console.log(day);
+
+let month = await userInput.question('Welke maand zijn we vandaag?');
+console.log('Het is vandaag ' + today + ' ' + day + ' ' + month + '.');
+
+
